b54c6e7fb2647dddd19bf0c117f611ed23fbc61b
taingk <taing_k@etna-alternance.net>
2019-05-07 16:35:53 +0200

    init buefy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Avatar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Avatar from './components/Avatar.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Avatar
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "~bulma/sass/utilities/_all";
+@import "styles/_variables.scss";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+import '@fortawesome/fontawesome-free/js/all.js'
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.use(Buefy, {
+    defaultIconPack: 'fas',
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
